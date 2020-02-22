bb93d0989d2b4477a43dcced77c8334074895910
LD <1048657976@qq.com>
2020-02-22 22:17:24 +0800

    仔细看注释

--- pytest/ldtest.py
+++ pytest/ldtest.py
@@ -13,7 +13,7 @@ driver=webdriver.Chrome()
 # driver.get('https://www.hongxiu.com/')
 driver.get('https://www.hongxiu.com/book/12115534503935401#Catalog')
 # 设置隐士等待
-driver.implicitly_wait(500)
+driver.implicitly_wait(10)
 # # 定位弹出页面
 # driver.find_element_by_id('j-guidePopup')
 # # 定位关闭符号，并点击
@@ -34,12 +34,20 @@ driver.implicitly_wait(500)
 # driver.find_element_by_xpath('//*[text()="含羞"]').click()
 # 定位所有的[data-rid]属性
 data_rid=driver.find_elements_by_css_selector('[data-rid]')
+
 b=[]
+# b 这个列表里是data-rid的值
 for i in data_rid:
     shu=i.get_attribute('data-rid')
     b.append(int(shu))
-# 获取属性的内容
+
 bb=list(set(b))
-print(bb)
-sleep(3)
-driver.quit()
+# 将列表去重并排序
+for ii in bb:
+    a=str(ii)
+    # 循环点击a 标签
+    driver.find_element_by_xpath('// li[ @ data - rid = '+a +']/a').click()
+#     这个循环里面我想实现循环定位点击并读取文本并写入
+quit()
+
+
