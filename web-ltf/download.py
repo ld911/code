7172e6d603cd59c5234847b83f33020ea834ee71
LD <1048657976@qq.com>
2020-02-26 22:46:44 +0800

    小说下载

--- web-ltf/download.py
+++ web-ltf/download.py
@@ -1,29 +1,26 @@
 # 导包
 import os
+from time import sleep
 
 from selenium import webdriver
 driver=webdriver.Chrome()
 # 打开小说的目录页
 driver.get('https://www.hongxiu.com/book/2810504401021002#Catalog')
 # 定位所有的目录
-url=driver.find_elements_by_css_selector('.volume .cf li a[href*="/"]')
-# print(url)
-http=[]
+url=driver.find_elements_by_css_selector('.volume .cf li a[href*="/chapter/"]')
+urls=[]
 for i in url:
-    # 获取所有目录中href的值添加到http中
-    http.append(i.get_attribute("href"))
-    # print(http)
+    # 获取所有目录中href的值添加到urls中
+    urls.append(i.get_attribute("href"))
+for id in urls:
+    driver.get(id)
+    text=driver.find_elements_by_css_selector('h3,p')
+    for txt in text:
+        book=txt.text
+        with open('./story/逃归.txt','a',encoding='utf-8') as t:
+            t.write(book)
 
-def down(id):
-    driver.get("https://www.hongxiu.com/book/"+id)
 
-for id in http:
-    down(id)
-    # text=driver.find_element_by_css_selector('h3,p').text
-    # if not os.mkdir('./story'):
-    #     os.mkdir('./story')
-    # newtext=open('./story/逃归.txt',mode='w',encoding='utf-8')
-    # oldtxt=newtext.write(text)
 
 
 
